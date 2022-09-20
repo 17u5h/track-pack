3851039e419f0a40713d2b7248ee89a7fa30e4ff
17u5h <102058870+17u5h@users.noreply.github.com>
2022-09-20 18:37:30 +0700

    стили переписаны в styled-components

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import * as S from "../styles";
 export function Logo() {
 	return (
 		<S.Logo>
-			<img src="../../public/img/logo.png" alt="logo"/>
+			<img src="./logo.png" alt="logo"/>
 		</S.Logo>
 	)
 }
